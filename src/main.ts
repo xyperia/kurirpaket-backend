cec68805b5eb1ad81eb18e8b1d3f3184fa8ec0f0
xyperia <septianparapak@gmail.com>
2024-02-05 10:02:24 +0700

    Disabled APM

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 
+  /*
   var apm = require('elastic-apm-node').start({
     serviceName: 'KurirPaket',
     serverUrl: 'http://10.30.100.16:8200',
     environment: 'Development'
   })
+  */
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
